b2eaac156f6624e32ee7427d0520903b83f42ff2
Shrinidhi Upadhyaya <162040697+Shrinidhi972004@users.noreply.github.com>
2025-07-18 12:47:06 +0530

    Update server_health_monitor.sh

--- server_health_monitor.sh
+++ server_health_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ---------- CONFIGURATION ----------
+# CONFIGURATION
 SNS_TOPIC_ARN="arn:aws:sns:us-east-1:245140949275:sever-health-monitor"
 CPU_THRESHOLD=1
 MEM_THRESHOLD=1
@@ -10,7 +10,7 @@ DISK_THRESHOLD=1
 SERVER_NAME=$(hostname)
 DATE_TIME=$(date '+%Y-%m-%d %H:%M:%S')
 
-# ---------- GET SYSTEM METRICS ----------
+# GET SYSTEM METRICS 
 
 CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 MEM_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
@@ -18,7 +18,7 @@ DISK_FREE=$(df / | tail -1 | awk '{print $4}')
 DISK_SIZE=$(df / | tail -1 | awk '{print $2}')
 DISK_FREE_PERCENT=$(awk "BEGIN {print ($DISK_FREE/$DISK_SIZE)*100}")
 
-# ---------- MONITOR AND ALERT ----------
+#  MONITOR AND ALERT 
 
 ALERT_MSG="Server: $SERVER_NAME 
 Date: $DATE_TIME
@@ -44,7 +44,7 @@ if (( $(echo "$DISK_FREE_PERCENT < $DISK_THRESHOLD" | bc -l) )); then
     TRIGGERED=1
 fi
 
-# ---------- SEND ALERT IF NEEDED ----------
+# SEND ALERT IF NEEDED 
 
 if [ $TRIGGERED -eq 1 ]; then
     echo -e "$ALERT_MSG"
@@ -56,4 +56,4 @@ else
     echo "All system health metrics are normal."
 fi
 
-# ---------- END ----------
+#  END 
